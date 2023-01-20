f1dd20285cd496da07b741ba7a2019e2e2ef2d4d
Vilmer <vilmer.strom@gmail.com>
2023-01-20 13:34:17 +0100

    rk4 vector version

--- Numerical-Solvers/RK4.jl
+++ Numerical-Solvers/RK4.jl
@@ -1,3 +1,11 @@
-function RK4(ẋ::function, xₙ, Δt)
+function RK4(ẋ::Function, xₙ, tₙ , Δt)
+
+    k₁ = ẋ(tₙ, xₙ)
+    k₂ = ẋ(tₙ + 0.5*Δt, xₙ + 0.5*Δt*k₁)
+    k₃ = ẋ(tₙ + 0.5*Δt, xₙ + 0.5*Δt*k₂)
+    k₄ = ẋ(tₙ + Δt, xₙ + Δt*k₃)
+
+    xₙ₊₁ = xₙ + 1/6*Δt*(k₁ + 2*k₂ + 2*k₃ + k₄)
     
+
 end
