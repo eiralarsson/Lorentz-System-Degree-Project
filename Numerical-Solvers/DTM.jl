b00fd87a359d500096dc02204b57df230e181a79
Eira Larsson <eiral@kth.se>
2023-01-20 17:07:08 +0100

    changed DTM function

--- Numerical-Solvers/DTM.jl
+++ Numerical-Solvers/DTM.jl
@@ -1,9 +1,17 @@
+using LinearAlgebra
+using Plots
+
 # Implementations of the differential transformation method (DTM) for lorenz system with 10 terms
-include("lorentz_system.jl")
-
-function DTM(θ,μ,β,x̄₀,Δt)
-    # differential transformation method
-    x̄_t = x̄₀
-
+# The DTM is a method for solving differential equations with a finite number of terms
+function DTM(p, x̄, terms)
+    θ=p[1]; μ=p[2]; β=p[3]
+    C = zeros(3, terms)
+    C[1,1] = x̄[1]; C[2,1] = x̄[2]; C[3,1] = x̄[3]
+    for i=2:terms
+        C[1,i] = (-θ*C[1,i-1] + θ*C[2,i-1])/i
+        C[2,i] = (C[1,i-1]*μ-sum(C[3,l]*C[3,i-1-l] for l in 1:i-1) - C[2,i-1])/i
+        C[3,i] = (sum(C[1,l]*C[2,i-1-l] for l in 1:i-1) - β*C[3,i-1])/i
+    end
+    
+    return C[:,end]
 end
-
--- Numerical-Solvers/euler_method.jl
+++ Numerical-Solvers/euler_method.jl
@@ -1,5 +1,3 @@
-include("lorentz_system.jl")
-
 function EulerForward(ẋ::Function,p,x̄,Δt)
     x̄ = x̄ + Δt*ẋ(p,x̄)
     return x̄
