eca43dcedf38fbbb32f8bedfdd31c47ea0d5be0c
Vilmer <vilmer.strom@gmail.com>
2023-01-20 13:51:17 +0100

    rk w/o time dependancy

--- Numerical-Solvers/RK4.jl
+++ Numerical-Solvers/RK4.jl
@@ -1,11 +1,12 @@
-function RK4(ẋ::Function, xₙ, tₙ , Δt)
+function RK4(ẋ::Function, p, xₙ, Δt)
 
-    k₁ = ẋ(tₙ, xₙ)
-    k₂ = ẋ(tₙ + 0.5*Δt, xₙ + 0.5*Δt*k₁)
-    k₃ = ẋ(tₙ + 0.5*Δt, xₙ + 0.5*Δt*k₂)
-    k₄ = ẋ(tₙ + Δt, xₙ + Δt*k₃)
+    k₁ = ẋ(p, xₙ)
+    k₂ = ẋ(p, xₙ + 0.5*Δt*k₁)
+    k₃ = ẋ(p, xₙ + 0.5*Δt*k₂)
+    k₄ = ẋ(p, xₙ + Δt*k₃)
 
     xₙ₊₁ = xₙ + 1/6*Δt*(k₁ + 2*k₂ + 2*k₃ + k₄)
-    
+
+    return xₙ₊₁
 
 end
