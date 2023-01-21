3f0e4a38db3f222f0b4fe6dbd44b334e8027aa99
Eira Larsson <eiral@kth.se>
2023-01-21 21:42:15 +0100

    now DTM works

--- Numerical-Solvers/DTM.jl
+++ Numerical-Solvers/DTM.jl
@@ -3,15 +3,23 @@ using Plots
 
 # Implementations of the differential transformation method (DTM) for lorenz system with 10 terms
 # The DTM is a method for solving differential equations with a finite number of terms
-function DTM(p, x̄, terms)
+function DTM(p, x̄, terms::Int64,Δt)
     θ=p[1]; μ=p[2]; β=p[3]
     C = zeros(3, terms)
     C[1,1] = x̄[1]; C[2,1] = x̄[2]; C[3,1] = x̄[3]
     for i=2:terms
-        C[1,i] = (-θ*C[1,i-1] + θ*C[2,i-1])/i
-        C[2,i] = (C[1,i-1]*μ-sum(C[3,l]*C[3,i-1-l] for l in 1:i-1) - C[2,i-1])/i
-        C[3,i] = (sum(C[1,l]*C[2,i-1-l] for l in 1:i-1) - β*C[3,i-1])/i
+        C[1,i] = θ*(C[2,i-1]-C[1,i-1])/i
+        C[2,i] = (C[1,i-1]*μ-sum(C[1,l]*C[3,i-l] for l in 1:i-1) - C[2,i-1])/i
+        C[3,i] = (sum(C[1,l]*C[2,i-l] for l in 1:i-1) - β*C[3,i-1])/i
     end
-    
-    return C[:,end]
+
+    x̄ = zeros(3)
+    for i=1:terms
+        x̄[1] = x̄[1] + C[1,i]*Δt^(i-1)
+        x̄[2] = x̄[2] + C[2,i]*Δt^(i-1)
+        x̄[3] = x̄[3] + C[3,i]*Δt^(i-1)
+    end
+
+    return x̄
 end
+
