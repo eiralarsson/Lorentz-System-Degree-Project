a7233cdb1fb4665129c4dfe27afc92eec16c43a4
vilmerstrom <vilmer.strom@gmail.com>
2023-04-03 16:30:08 +0200

    test

--- Time-Dep-Parameters/PlotFunctions.jl
+++ Time-Dep-Parameters/PlotFunctions.jl
@@ -1,5 +1,5 @@
 
-function PlotNucleusPlot(p,solutions)
+function plot_nucleus_plot(p,solutions)
     for X = solutions
         scatter!(p, X[1,[1]], X[2,[1]], X[3,[1]], color=:blue)
         scatter!(p, X[1,[N]], X[2,[N]], X[3,[N]], color=:red)
@@ -8,28 +8,28 @@ function PlotNucleusPlot(p,solutions)
     return p
 end
 
-function PlotTrajectories(p,solutions; label = "")
+function plot_trajectories(p,solutions; label = "")
     for X = solutions
         plot!(p, X[1,:], X[2,:], X[3,:], label=label) 
     end
     return p
 end
 
-function PlotTrajectoriesInterpolated(p,solutions)
+function plot_trajectories_interpolated(p,solutions)
     for X = solutions
         plot!(p, X, vars=(1,2,3))
     end
     return p
 end
 
-function PlotTrajectoriesOneVariable(p,solutions, index)
+function plot_trajectories_one_variable(p,solutions, index)
     for X = solutions
         plot!(p, X[index,:])
     end
     return p
 end
 
-function PlotPipPlot(M,Δt;title="Correlation plot",clim=(-1,1),cmap=:grayC, yaxis=false)
+function plot_pip_plot(p,M,Δt;title="Correlation plot",clim=(-1,1),cmap=:grayC, yaxis=false)
     r,c = size(M)
     y = [1:1:r]
     x = [0:Δt:Δt*(c-1)]
